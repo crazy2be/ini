96d1c1a995b7a502994dc4dbc43f0a36f105279f
crazy2be <crazy1be@gmail.com>
2012-03-10 19:18:58 -0500

    Apparently Load() and Save() were not returning errors even when they encountered an error. I honestly can't beleive how bad my error handling was looking back...

--- ini.go
+++ ini.go
@@ -5,19 +5,17 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	"path"
 )
 
 // Load an ini file. Pass a filename, returns a map of all of the name=value pairs within the file, and an error if applicable.
 // TODO: Read by line, rather than buffering the whole file into memory.
-func Load(filename string) (settings map[string]string, err error) {
-	settings = make(map[string]string, 10)
+func Load(filename string) (map[string]string, error) {
+	settings := make(map[string]string, 10)
 	fileContents, err := ioutil.ReadFile(filename)
 	if err != nil {
-		log.Printf("Failed to read %s: %s.\n", filename, err)
-		return
+		return nil, err
 	}
 	lines := bytes.Split(fileContents, []byte("\n"))
 	for _, line := range lines {
@@ -28,26 +26,24 @@ func Load(filename string) (settings map[string]string, err error) {
 		}
 		settings[string(splitLine[0])] = string(splitLine[1])
 	}
-	return
+	return settings, nil
 }
 
 // Save a map of settings to filename in name=value format. Returns nil on success, error otherwise. Creates any necessary directories to save the file according to the path given.
-func Save(filename string, settings map[string]string) (err error) {
+func Save(filename string, settings map[string]string) (error) {
 	dirname, _ := path.Split(filename)
-	err = os.MkdirAll(dirname, 0755)
+	err := os.MkdirAll(dirname, 0755)
 	if err != nil {
-		log.Println("Error creating directory to save ini file", filename, ":", err)
-		return
+		return err
 	}
 
 	var file *os.File
 	file, err = os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
-		log.Printf("Failed to open %s, %s.\n", filename, err)
-		return
+		return err
 	}
 	for key, value := range settings {
 		fmt.Fprintf(file, "%s=%s\n", key, value)
 	}
-	return
+	return nil
 }
